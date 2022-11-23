2bf897c63d533b648023fdd03029e72f2380ae25
antoniozamvaz <antoniozamvaz@DAW-15>
2022-11-23 20:09:49 +0100

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P1_ASP.Models;
+using System.Diagnostics;
+
+namespace P1_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Vname = "Autor: Antonio Zamacona";
+            ViewBag.V2 = "Voy a ser un buen programador de ASPx.NET";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
