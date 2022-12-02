1f1f08b066733de82b8975d1d38326a36855e5f1
Antonio Zamacona Vazquez <antoniozamvaz@campus.monlau.com>
2022-12-02 19:45:18 +0100

    Añadidas pestañas de la barra de navegación

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,14 +27,24 @@ namespace P1_ASP.Controllers
             
             return View();
         }
-
+        public IActionResult Experience() {
+            return View();
+        }
         public IActionResult Proyectos()
         {
             ViewBag.Vproyecto = new RepositoryOfProjects().GetProjects();
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult AcademicExperience() {
+            return View();
+        }
+        public IActionResult TechnicalKnowledge() {
+            return View();
+        }
+        public IActionResult CurriculumVitae() {
+            return View();
+        }
+       [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
---
+++ Services/RepositoryOfJobs.cs
@@ -0,0 +1,43 @@
+﻿using P1_ASP.Models;
+
+namespace P1_ASP.Services
+{
+    public class RepositoryOfProjects
+    {
+        public List<ClassProjects> GetProjects() {
+
+            return new List<ClassProjects>() {
+                new ClassProjects {
+                Title = "ASP.net",
+                Description = "WEB realizado en ASP.NET Core",
+                Link = "https://amazon.com",
+                ImageURL = "/img/1366_2000.jpg"
+                },
+                 new ClassProjects {
+                Title = "Java Script",
+                Description = "Animación realizado en JS",
+                Link = "https://Monlau.com",
+                ImageURL = "/img/q79j6x3o.bmp"
+                },
+                  new ClassProjects {
+                Title = "Java Script",
+                Description = "Animación realizado en JS",
+                Link = "https://Monlau.com",
+                ImageURL = "/img/present.bmp"
+                },
+                   new ClassProjects {
+                Title = "Java Script",
+                Description = "Animación realizado en JS",
+                Link = "https://Monlau.com",
+                ImageURL = "/img/retro.bmp"
+                },
+                    new ClassProjects {
+                Title = "Java Script",
+                Description = "Animación realizado en JS",
+                Link = "https://Monlau.com",
+                ImageURL = "/img/grid_bg.bmp"
+                }
+            };
+        }
+    }
+}
